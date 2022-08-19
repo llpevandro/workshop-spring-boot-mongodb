e8e7063c7892a58b694192727c896c28368b85d4
Evandro <llpevandrodev@gmail.com>
2022-08-19 12:53:50 -0300

    Referencing user posts

--- src/main/java/com/llpevandro/workshopmongo/domain/Users.java
+++ src/main/java/com/llpevandro/workshopmongo/domain/Users.java
@@ -1,26 +1,31 @@
 package com.llpevandro.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
 @Document(collection = "users")
-public class Users implements Serializable{
+public class Users implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
-	
+
 	private String name;
 	private String email;
-	
-	public Users () {		
+
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+
+	public Users() {
 	}
 
-	public Users(String id, String name, String email) {		
+	public Users(String id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -50,6 +55,16 @@ public class Users implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
+	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -66,8 +81,5 @@ public class Users implements Serializable{
 		Users other = (Users) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 
 }
