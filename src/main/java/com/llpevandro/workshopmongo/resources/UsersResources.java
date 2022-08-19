d48851d417e6b64ca78942d88d2c0b1294e92d8e
Evandro <llpevandrodev@gmail.com>
2022-08-19 11:36:55 -0300

    User find by id

--- src/main/java/com/llpevandro/workshopmongo/resources/UsersResources.java
+++ src/main/java/com/llpevandro/workshopmongo/resources/UsersResources.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,19 @@ public class UsersResources {
 
 	@Autowired
 	private UsersService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UsersDTO>> findAll() {
-		
-		List <Users> list = service.findAll();
-		List <UsersDTO> listDto = list.stream().map(x -> new UsersDTO(x)).collect(Collectors.toList());
+
+		List<Users> list = service.findAll();
+		List<UsersDTO> listDto = list.stream().map(x -> new UsersDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UsersDTO> findById(@PathVariable String id) {
+		Users obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UsersDTO(obj));
+	}
+
 }
--- src/main/java/com/llpevandro/workshopmongo/services/UsersService.java
+++ src/main/java/com/llpevandro/workshopmongo/services/UsersService.java
@@ -1,21 +1,27 @@
 package com.llpevandro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.llpevandro.workshopmongo.domain.Users;
 import com.llpevandro.workshopmongo.repository.UsersRepository;
+import com.llpevandro.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UsersService {
-	
-		@Autowired
-	private UsersRepository repo;
-		
-		
-	public List<Users> findAll(){
-		return repo.findAll();		
+
+	@Autowired
+	private UsersRepository repo;	
+
+	public List<Users> findAll() {
+		return repo.findAll();
+	}
+
+	public Users findById(String id) {
+		Optional<Users> obj = repo.findById(id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
