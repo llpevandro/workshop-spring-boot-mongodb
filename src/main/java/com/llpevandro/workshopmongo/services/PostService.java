18b8c429cfe892ff647e864b6c7fe8b10dea27a6
Evandro <llpevandrodev@gmail.com>
2022-08-19 13:01:34 -0300

    Getting a post by id (need fix)

---
+++ src/main/java/com/llpevandro/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.llpevandro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.llpevandro.workshopmongo.domain.Post;
+import com.llpevandro.workshopmongo.domain.Users;
+import com.llpevandro.workshopmongo.repository.PostRepository;
+import com.llpevandro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+
+}
